5e94923781117ff742789f7432cacb3660bf93af
Claudiasals <salsiniclaudia@gmail.com>
2025-11-27 03:06:06 +0100

    - create funzioni per non ripetere il codice per creare un nuovo messaggio e una per mostrarlo in pagina.
    - resa dinamica la funzione che riporta in chat i essaggi aggiungendo i parametri messageTexe e messageType per poter aggiungere i messaggi sia in entrata che in uscita.

--- script.js
+++ script.js
@@ -18,7 +18,9 @@ const messages = [
         type: "received",
         text: "Hei, io tutto bene, grazie. E tu come stai?? ",
         time: "20:27"
-    }
+    },
+
+
 ];
 
 
@@ -29,23 +31,66 @@ const messages = [
 chatBox.innerHTML = "";
 
 // Per ciascuno dei messaggi creami una variabiule messagge
-for (const message of messages) {
-    chatBox.innerHTML += `
-    <div class="chat-row ${message.type}">
-        <div class="chat-message">
-            <p> ${message.text}</p>
-            <time datetime="${message.time}">
-            ${message.time}
-            </time>
-        </div>
-    </div>`
-}
+showMessages();
 
 
 // @ OPERAZIONI DI INTERAZIONE CON L'UTENTE
 
 // al click del bottone invia il msg inserito nell'input dall'utente
-button.addEventListener("click", function(){
+button.addEventListener("click", function () {
     const insertedText = input.value; // prendo il testo inserito nell'input
     console.log(insertedText)
-})
+
+    // controllo che l'input non sia vuoto
+    if (!insertedText) return; // se l’input è vuoto, la funzione si interrompe subito
+
+    addMessage();
+    // svuuoto chat
+    chatBox.innerHTML = "";
+
+    // aggiungo il nuovo messaggio alla lista dei messaggi
+    showMessages();
+
+    // svuoto la casella input dopo aver inviato il messaggio
+    input.value = ""
+    // e riporto il cursore nell'input
+    input.focus()
+
+});
+
+// ? FUNZIONI UTILI
+
+function showMessages() {
+    // svuuoto chat
+    chatBox.innerHTML = "";
+
+    // aggiungo il nuovo messaggio alla lista dei messaggi
+    for (const message of messages) {
+        chatBox.innerHTML += `
+       <div class="chat-row ${message.type}">
+           <div class="chat-message">
+               <p> ${message.text}</p>
+               <time datetime="${message.time}">
+               ${message.time}
+               </time>
+           </div>
+       </div>`
+    }
+}
+
+function addMessage(messageType, messageText) {
+    // creo un nuovo messaggio
+    const newMessage = {
+        type: messageType,
+        text: messageText,
+        time: new Date().toLocaleString()
+    }
+    // aggiungo msg alla lista messaggi
+    messages.push(newMessage) // il nuovo oggetto viene aggiunto all'array come 3 oggetto.
+    // mostra i msg in chat
+    showMessages()
+}
+
+// esempio per capire la logica:
+addMessage("received", "allora quando ci vediamo?")
+addMessage("sent", "venerdì sera??")
